63da8056afcdd9c63745c0a4b9d04c4e48d8d5f9
NRayya <82588017+NRayya@users.noreply.github.com>
2025-08-15 18:07:54 +0200

    Deploy website - based on 2948cacb4598915071fb665ce984b51bfae44b1b

---
+++ assets/js/5e95c892.5fcb42e8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkworkshops=self.webpackChunkworkshops||[]).push([[9647],{7121:(s,e,r)=>{r.r(e),r.d(e,{default:()=>p});r(6540);var u=r(4164),a=r(5500),c=r(7559),o=r(2831),h=r(1656),n=r(4848);function p(s){return(0,n.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,n.jsx)(h.A,{children:(0,o.v)(s.route.routes)})})}}}]);
